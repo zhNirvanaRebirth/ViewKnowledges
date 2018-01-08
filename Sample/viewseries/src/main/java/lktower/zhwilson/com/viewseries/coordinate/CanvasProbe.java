93c84f444ef61c770518b680ee067e83595d024e
zhouhang <2501213087@qq.com>
2018-01-08 18:14:58 +0800

    Canvas对绘制的辅助

--- Sample/viewseries/src/main/java/lktower/zhwilson/com/viewseries/coordinate/CanvasProbe.java
+++ Sample/viewseries/src/main/java/lktower/zhwilson/com/viewseries/coordinate/CanvasProbe.java
@@ -3,13 +3,16 @@ package lktower.zhwilson.com.viewseries.coordinate;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Picture;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.graphics.drawable.PictureDrawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -24,6 +27,13 @@ public class CanvasProbe extends View {
     private int width;
     private int height;
     private Paint paint;
+    private Paint textPaint;
+
+    private Camera camera;
+    private Matrix matrix;
+    private float density;
+    private float[] values = new float[9];
+    private float deg = 0;
 
     public CanvasProbe(Context context) {
         this(context, null);
@@ -36,10 +46,16 @@ public class CanvasProbe extends View {
     public CanvasProbe(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initPaint();
+        camera = new Camera();
+        matrix = new Matrix();
+        density = getResources().getDisplayMetrics().density;
     }
 
     private void initPaint() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(Color.BLACK);
     }
 
     @Override
@@ -58,7 +74,7 @@ public class CanvasProbe extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
-        canvas.translate(width * 1.0f / 2, height * 1.0f / 2);
+//        canvas.translate(width * 1.0f / 2, height * 1.0f / 2);
 
 //        canvas.drawLine(0, -height*1.0f/2, 0, height*1.0f/2, paint);
 //        canvas.drawLine(-width*1.0f/2, 0, width*1.0f/2, 0, paint);
@@ -127,9 +143,85 @@ public class CanvasProbe extends View {
 //        canvas.concat(matrix);
 //        canvas.drawBitmap(icon, new Matrix(), new Paint());
 //        canvas.restore();
-        Rect src = new Rect(0, 0, (int) (icon.getWidth() * 1.0f / 2), (int) (icon.getHeight() * 1.0f / 2));
-        RectF dst = new RectF(100, 100, icon.getWidth() + 100, icon.getHeight() + 100);
-        canvas.drawBitmap(icon, src, dst, paint);
+//        Rect src = new Rect(0, 0, (int) (icon.getWidth() * 1.0f / 2), (int) (icon.getHeight() * 1.0f / 2));
+//        RectF dst = new RectF(100, 100, icon.getWidth() + 100, icon.getHeight() + 100);
+//        canvas.drawBitmap(icon, src, dst, paint);
+        canvas.save();
+        canvas.translate(width * 1.0f / 2, height * 1.0f / 2);
+        canvas.drawLine(0, -1000, 0, 1000, textPaint);
+        canvas.drawLine(-800, 0, 800, 0, textPaint);
+        canvas.restore();
+//        canvas.save();
+//        canvas.clipRect(-icon.getWidth()/2, 0, icon.getWidth()/2, icon.getHeight()/2);
+//        canvas.drawBitmap(icon, -icon.getWidth()/2, -icon.getHeight()/2, paint);
+//        canvas.restore();
+
+//        canvas.save();
+//        Path path = new Path();
+//        path.addCircle(0, 0, 100, Path.Direction.CW);
+////        canvas.clipPath(path);
+//        canvas.clipPath(path, Region.Op.DIFFERENCE);
+//        canvas.drawBitmap(icon, -icon.getWidth()/2, -icon.getHeight()/2, paint);
+//        canvas.restore();
+
+//        canvas.save();
+//        canvas.translate(width * 1.0f / 2, height * 1.0f / 2);
+//        canvas.clipRect(-icon.getWidth()/2, -icon.getHeight()/2, icon.getWidth()/2, 0);
+//        canvas.drawBitmap(icon, -icon.getWidth()/2, -icon.getHeight()/2, paint);
+//        canvas.restore();
+//
+//        canvas.save();
+//        Camera camera = new Camera();
+//        Matrix matrix = new Matrix();
+//        matrix.reset();
+//        camera.save();
+//        camera.rotateX(45);
+//        camera.getMatrix(matrix);
+//        camera.restore();
+//        matrix.postTranslate(width*1.0f/2, height*1.0f/2);
+//        canvas.concat(matrix);
+//        canvas.clipRect(-icon.getWidth()/2, 0, icon.getWidth()/2, icon.getHeight()/2);
+//        canvas.drawBitmap(icon, -icon.getWidth()/2, -icon.getHeight()/2, paint);
+//        canvas.restore();
+
+        canvas.save();
+        matrix.reset();
+        camera.save();
+        camera.rotateX(60);
+        camera.getMatrix(matrix);
+        camera.restore();
+        matrix.getValues(values);
+        values[6] = values[6] / density;
+        values[7] = values[7] / density;
+        matrix.postTranslate(width * 1.0f / 2, height * 1.0f / 2);
+        canvas.concat(matrix);
+        canvas.drawBitmap(icon, -icon.getWidth() / 2, -icon.getHeight() / 2, paint);
+        canvas.restore();
+
+        canvas.save();
+        matrix.reset();
+        camera.save();
+        camera.rotateX(60);
+        float rd = deg < 90 ? -deg : 180 - deg;
+        camera.rotateY(rd);
+        camera.getMatrix(matrix);
+        camera.restore();
+        matrix.getValues(values);
+        values[6] = values[6] / density;
+        values[7] = values[7] / density;
+        matrix.postTranslate(width * 1.0f / 2, height * 1.0f / 2);
+        canvas.concat(matrix);
+        if (deg < 90)
+            canvas.clipRect(0, -icon.getHeight() / 2, icon.getWidth() / 2, icon.getHeight() / 2);
+        else canvas.clipRect(-icon.getWidth() / 2, -icon.getHeight() / 2, 0, icon.getHeight() / 2);
+        canvas.drawBitmap(icon, -icon.getWidth() / 2, -icon.getHeight() / 2, paint);
+        canvas.restore();
+
+        deg += 4;
+        if (deg >= 180) {
+            deg = 0;
+        }
+        postInvalidate();
     }
 
 //    Picture picture = new Picture();
